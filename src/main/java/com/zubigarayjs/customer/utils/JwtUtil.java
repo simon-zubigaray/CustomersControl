6f0e4ca116c7e2086a2934ae7f218823620cdc14
Zubigaray, Juan Simon <zubigarayjuansimon@gmail.com>
2024-08-20 11:21:14 -0300

    Verificar token con JWT

--- src/main/java/com/zubigarayjs/customer/utils/JwtUtil.java
+++ src/main/java/com/zubigarayjs/customer/utils/JwtUtil.java
@@ -1,7 +1,9 @@
 package com.zubigarayjs.customer.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.zubigarayjs.customer.model.User;
 
 import java.util.Date;
@@ -9,10 +11,9 @@ import java.util.Date;
 public class JwtUtil {
 
     private static final String SECRET_KEY = "secret_key_is_java123";
+    private static final Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 
     public static String generateToken(User user){
-        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-
         String token = JWT.create()
                             .withIssuer("Juan Simon Zubigaray")
                             .withClaim("userId", user.getId_user())
@@ -22,9 +23,14 @@ public class JwtUtil {
         return token;
     }
 
-    //public User getUserByToken(String token){
-
-    //}
+    public static String getUserIdByToken(String token){
+        JWTVerifier verifier = JWT.require(algorithm)
+                                    .withIssuer("Juan Simon Zubigaray")
+                                    .build();
+        DecodedJWT decoded = verifier.verify(token);
+        String userId = decoded.getClaim("userId").toString();
+        return userId;
+    }
 
     private static Date getExpiresDate(){
         return new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24) ); //1 día
